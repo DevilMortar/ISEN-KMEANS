00b5c6903f11ea10e2eae3ecce73afdfa22869f8
DevilMortar <devilmortar0159@gmail.com>
2022-04-25 20:29:24 +0200

    finished project

--- index.py
+++ index.py
@@ -1,5 +1,6 @@
 from ast import Or
 import csv
+from email import header
 import drawLib
 
 def loadCSV(file):
@@ -9,6 +10,14 @@ def loadCSV(file):
         for row in content:
             r.append(row)
     return r
+
+def saveCSV(file, data):
+    with open('file/' + file, 'w', newline='') as f:
+        writer = csv.writer(f, delimiter=',')
+        header = ['x', 'y', 'centroid_x', 'centroid_y']
+        writer.writerow(header)
+        for row in data:
+            writer.writerow(row)
 
 def cleanData(data):
     # Remove the first line
@@ -24,6 +33,26 @@ def copyListIntoList(origin, destination):
     for i in range(len(origin)):
         destination[i] = origin[i]
     
+def assignCluster(data, centroids):
+    clusters = [[] for i in range(len(centroids))]
+    for point in data:
+        closest = 0
+        for i in range(1, len(centroids)):
+            if distance(point, centroids[i]) < distance(point, centroids[closest]):
+                closest = i
+        clusters[closest].append(point)
+    return clusters
+
+def updateCentroids(clusters):
+    centroids = []
+    for cluster in clusters:
+        if len(cluster) == 0:
+            pass
+        else:
+            # Calculate the middle of the cluster
+            centroids.append((sum(cluster[i][0] for i in range(len(cluster))) / len(cluster), sum(cluster[i][1] for i in range(len(cluster)))/ len(cluster)))
+    return centroids
+
 
 def kMeans(data, k, maxIterations=100):
     iteration = 0
@@ -31,50 +60,43 @@ def kMeans(data, k, maxIterations=100):
     centroids = []
     for i in range(k):
         centroids.append(data[i])
-    
     while True:
-        # Create a list of lists
-        clusters = [[] for i in range(k)]
-        # Assign each point to the closest centroid
-        w = -1
-        for point in data:
-            w+=1
-            print(w)
-            # Find the closest centroid
-            closest = 0
-            for i in range(1, k):
-                if distance(point, centroids[i]) < distance(point, centroids[closest]):
-                    closest = i
-            # Add the point to the closest cluster
-            clusters[closest].append(point)
+        # Assign the clusters
+        clusters = assignCluster(data, centroids)
         # Calculate the new centroids
-        print("New centroids:")
-        newCentroids = []
-        for cluster in clusters:
-            if len(cluster) == 0:
-                pass
-            else:
-                # Calculate the middle of the cluster
-                newCentroids.append((sum(cluster[i][0] for i in range(len(cluster))) / len(cluster), sum(cluster[i][1] for i in range(len(cluster)))))
-        # Check if the centroids have changed
+        newCentroids = updateCentroids(clusters)
+        # Check if the centroids have changed or if the maximum number of iterations has been reached
         if newCentroids == centroids or iteration == maxIterations:
             break
         # Update the centroids
         copyListIntoList(newCentroids, centroids)
-        print(centroids)
         iteration += 1
-        
-    return centroids
+    # create exporting list
+    list = []
+    for i in range(len(clusters)):
+        for point in clusters[i]:
+            newPoint = []
+            newPoint.append(point[0])
+            newPoint.append(point[1])
+            newPoint.append(centroids[i][0])
+            newPoint.append(centroids[i][1])
+            list.append(newPoint)
+    return list
+
+      
                 
 
     
 
     
 
-list = cleanData(loadCSV('mock_2d_data.csv'))
+data = cleanData(loadCSV('2d_data.csv'))
 #list = cleanData(loadCSV('2d_data.csv'))
 #drawLib.draw2D(list, size=10, drawLinks=True)
-print(kMeans(list, 3, maxIterations=100))
+list = kMeans(data, 20, maxIterations=200)
+drawLib.draw2D(list, size=10, drawLinks=True)
+saveCSV('export.csv', list)
+
 
 
 
